b94cbdacec2cdc495f1b54321bda9d4e9f49ca5f
neroaugustus1 <neroaugustus1@gmail.com>
2015-08-12 19:42:15 +0200

    Delete data.js

--- data.js
+++
@@ -1,8 +0,0 @@
-var DataLayer = (function () {
-    function DataLayer(name) {
-        this.name = name;
-    }
-    return DataLayer;
-})();
-exports.DataLayer = DataLayer;
-//# sourceMappingURL=data.js.map
